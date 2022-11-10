bc1ed2df2bf5be3ead19d64e28968340b60342b0
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-11-11 00:53:55 +0530

    pubspec.yaml and plugins updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:first_project/pages/HomePage.dart';
+
+// import 'package:first_project/pages/company/Products/update_product.dart';
+// import 'package:first_project/pages/company/Products/add_product.dart';
+import 'package:first_project/pages/company/Products/product_list.dart';
+// import 'package:first_project/pages/company/Cutomers/pending_customers.dart';
+// import 'package:first_project/pages/company/Cutomers/approved_customers.dart';
+// import 'package:first_project/pages/company/Cutomers/generateReport.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +16,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-      routes: {"/": (context) => HomePage()},
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(color: Colors.green),
+      ),
+      // routes: {"/": (context) => HomePage()},
+      // routes: {"/": (context) => ApprovedCustomers()},
+      // routes: {"/": (context) => const AddProduct()},
+      // routes: {"/": (context) => const UpdateProduct()},
+      routes: {"/": (context) => const AllProducts()},
+      // routes: {"/": (context) => const PendingCustomers()},
+      // routes: {"/": (context) => const DeclinedCustomers()},
+      // routes: {"/": (context) => const ApprovedCustomers()},
+      // routes: {"/": (context) => const GenerateReport()},
     );
   }
 }
