5903430cbed84340faa505afada26f7d1954de81
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-11-11 00:48:04 +0530

    Declined Customers Created

---
+++ lib/model/product.dart
@@ -0,0 +1,19 @@
+class ProductModel {
+  late String name;
+  late String code;
+  late String image;
+  late String category;
+  late String subcategory;
+  late String price;
+  late String description;
+
+  ProductModel({
+    required this.name,
+    required this.code,
+    required this.image,
+    required this.category,
+    required this.subcategory,
+    required this.price,
+    required this.description,
+  });
+}
