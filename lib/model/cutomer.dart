8661e9586180f98d548515a7a137632b12554ce4
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-11-11 00:46:35 +0530

    Customer Model Created

---
+++ lib/model/cutomer.dart
@@ -0,0 +1,13 @@
+class CustomerModel {
+  late String name;
+  late String email;
+  late String address;
+  late String status;
+
+  CustomerModel({
+    required this.name,
+    required this.email,
+    required this.address,
+    required this.status
+  });
+}
